053aa210fd2a2b54aabc34c978193d0a149b5c13
nooglersoon <nooglersoon@outlook.co.id>
2023-02-19 13:34:46 +0700

    Initial Commit

---
+++ SafarKit/SafarKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafarKitApp.swift
+//  SafarKit
+//
+//  Created by Fauzi Achmad B D on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SafarKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
